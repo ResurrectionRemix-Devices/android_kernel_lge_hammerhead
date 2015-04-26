50028fddb2b007b4eac8fddfb1612d4be49a354d
Tyler Dunn <robotdude@att.net>
2015-04-25 23:54:03 -0400

    mdss_mdp_pp: Fix GCC 5.0 error
    Conflicts:
    	drivers/video/msm/mdss/mdss_mdp_pp.c

--- drivers/video/msm/mdss/mdss_mdp_pp.c
+++ drivers/video/msm/mdss/mdss_mdp_pp.c
@@ -303,6 +303,19 @@ static void pp_ad_cfg_lut(char __iomem *offset, u32 *data);
 
 static u32 last_sts, last_state;
 
+static inline int linear_map(int in, int *out, int in_max, int out_max)
+{
+	if (in < 0 || !out || in_max <= 0 || out_max <= 0)
+		return -EINVAL;
+	*out = ((in * out_max) / in_max);
+	pr_debug("in = %d, out = %d, in_max = %d, out_max = %d\n",
+		in, *out, in_max, out_max);
+	if ((in > 0) && (*out == 0))
+		*out = 1;
+	return 0;
+
+}
+
 int mdss_mdp_csc_setup_data(u32 block, u32 blk_idx, u32 tbl_idx,
 				   struct mdp_csc_cfg *data)
 {
